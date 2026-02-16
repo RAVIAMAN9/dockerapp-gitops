80378c8d43bba099140af83c1252dc1b75cf20e7
amank <ravikumarjha25@gmail.com>
2026-02-16 14:34:32 +0530

    Initial commit

---
+++ src/main/java/com/example/dockerapp_gitops/DockerappGitopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerapp_gitops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerappGitopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerappGitopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dockerapp_gitops/DockerappGitopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerapp_gitops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerappGitopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
